e4277eacbb440b8b42c7da1469be884fe02abdc6
mnc13 <masnunnuha@iut-dhaka.edu>
2025-03-10 13:44:07 +0600

    unauth and not found logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,25 +3,25 @@ import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
 import AdminLayout from "./components/admin-view/layout";
-//import AdminDashboard from "./pages/admin-view/dashboard";
-//import AdminProducts from "./pages/admin-view/products";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminProducts from "./pages/admin-view/products";
 //import AdminOrders from "./pages/admin-view/orders";
 //import AdminFeatures from "./pages/admin-view/features";
 //import ShoppingLayout from "./components/shopping-view/layout";
 //import NotFound from "./pages/not-found";
-//import ShoppingHome from "./pages/shopping-view/home";
+import ShoppingHome from "./pages/shopping-view/home";
 //import ShoppingListing from "./pages/shopping-view/listing";
 //import ShoppingCheckout from "./pages/shopping-view/checkout";
 //import ShoppingAccount from "./pages/shopping-view/account";
 import CheckAuth from "./components/common/check-auth";
-//import UnauthPage from "./pages/unauth-page";
+import UnauthPage from "./pages/unauth-page";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { checkAuth } from "./store/auth-slice";
 import { Skeleton } from "@/components/ui/skeleton";
 //import PaymentSuccessPage from "./pages/shopping-view/payment-success";
 //import OrderSuccessPage from "./pages/shopping-view/order-success";
-//import SearchProducts from "./pages/shopping-view/search";
+import SearchProducts from "./pages/shopping-view/search";
 
 function App() {
   const { user, isAuthenticated, isLoading } = useSelector(
@@ -71,9 +71,24 @@ function App() {
           }
         >
           
+        <Route path="dashboard" element={<AdminDashboard />} />
+        <Route path="products" element={<AdminProducts />} />
         </Route>
-       
-        
+
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
+         <Route path="home" element={<ShoppingHome />} />
+         {/*<Route path="listing" element={<ShoppingListing />} />*/}
+         <Route path="search" element={<SearchProducts />} />
+        </Route>
+        <Route path="/unauth-page" element={<UnauthPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
